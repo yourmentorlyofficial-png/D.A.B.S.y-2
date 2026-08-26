3febaf4736ce0d0a3c88be4d3dfd1b547e93dbd1
yourmentorlyofficial-png <your.mentorly.official@gmail.com>
2026-08-26 10:37:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,9 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
