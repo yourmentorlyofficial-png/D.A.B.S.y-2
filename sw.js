cb5aac038406b5299666156e0eea9b2bde320161
yourmentorlyofficial-png <your.mentorly.official@gmail.com>
2026-08-26 15:31:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,111 @@
-const CACHE_NAME = 'dabsy-capsule-v1';
+const CACHE_NAME = "dabsy-v2-clean";
+
+
 const ASSETS = [
-  './index.html',
-  './styles.css',
-  './app.js',
-  './manifest.json',
-  './icon.svg'
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json"
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
-  );
-  self.clients.claim();
-});
+self.addEventListener(
+  "install",
+  event => {
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request).catch(() => {
-        if (event.request.mode === 'navigate') {
-          return caches.match('./index.html');
-        }
-      });
-    })
-  );
-});
+    event.waitUntil(
+
+      caches
+        .open(CACHE_NAME)
+        .then(cache => {
+
+          return cache.addAll(
+            ASSETS
+          );
+
+        })
+
+    );
+
+    self.skipWaiting();
+
+  }
+);
+
+
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .keys()
+        .then(keys => {
+
+          return Promise.all(
+
+            keys
+              .filter(
+                key =>
+                  key !== CACHE_NAME
+              )
+
+              .map(
+                key =>
+                  caches.delete(key)
+              )
+
+          );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    event.respondWith(
+
+      fetch(event.request)
+
+        .then(response => {
+
+          const copy =
+            response.clone();
+
+          caches
+            .open(CACHE_NAME)
+            .then(cache => {
+
+              cache.put(
+                event.request,
+                copy
+              );
+
+            });
+
+          return response;
+
+        })
+
+        .catch(() => {
+
+          return caches.match(
+            event.request
+          );
+
+        })
+
+    );
+
+  }
+);
